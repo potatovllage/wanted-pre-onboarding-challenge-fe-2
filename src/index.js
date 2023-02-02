b90a6c3ab9c5d16667baac6e0b4e9550fb3e419b
Yeong joon <bluehome8626@naver.com>
2023-02-02 14:30:36 +0900

    ⚡️ :: 할 일 추가하기

---
+++ src/index.js
@@ -0,0 +1,33 @@
+/*
+필수 요구사항
+아래의 Todo 앱 요구사항을 참고하여
+필요한 데이터를 모두 모델링한다.
+사용되는 모든 함수를 선언부만 만든다.
+함수 및 클래스의 내부는 구현하지 않습니다.
+JSDoc을 활용해 문서화한다.
+GitHub Page를 활용해 JSDoc 정적 페이지를 배포한다.
+
+Todo {
+  아이디(required),
+  내용(required),
+  완료여부(required),
+  카테고리(required),
+  태그들(optional),
+}
+
+*/
+
+/**
+ * @description 할 일 추가하기
+ * @param {string} title
+ * @param {string || null} content
+ * @param {string} category
+ * @param {boolean} whether
+ * @param {string || null} tag
+ * @returns {number} post_id
+ */
+const Create = (title, content, category, whether, tag) => {};
+
+/**
+ *
+ */
